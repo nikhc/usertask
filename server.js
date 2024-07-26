a6866384d008913b1dbad8eb0e2c5da5d7cd622a
Nikhil yadav <64726741+nikhc@users.noreply.github.com>
2024-07-26 11:00:08 +0530

    hhbdshdj

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path=require("path")
 const candidateModel = require('./userModel'); 
 
 const app = express();
-const port = 5000;
+const  port=process.env.port||5000
 
 // Use CORS middleware
 app.use(cors());
